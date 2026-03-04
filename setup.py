9a6e83fd4ce542e8a47f1e30b53a8fb499cc4352
Sulaiman <sulabaziz09@gmail.com>
2026-03-04 19:15:19 +0700

    Release 1.4.4
    Release 1.4.4 (fix TOML)

--- setup.py
+++ setup.py
@@ -6,42 +6,6 @@
 # of the Apache Software License. See the accompanying LICENSE file
 # for more information.
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="bloodpengu-python",
-    version="1.4.3",
-    description="Data collector in Python for BloodPengu APM",
-    long_description=open("README.md").read() if __import__("os").path.exists("README.md") else "",
-    long_description_content_type="text/markdown",
-    author="byt3n33dl3",
-    author_email="",
-    url="https://github.com/byt3n33dl3/gxc-BloodPengu.py",
-    project_urls={
-        "Source": "https://github.com/byt3n33dl3/gxc-BloodPengu.py",
-        "Tracker": "https://github.com/byt3n33dl3/gxc-BloodPengu.py/issues",
-    },
-    license="Apache-2.0",
-    python_requires=">=3.7",
-    install_requires=[
-        "paramiko>=3.0.0",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    py_modules=["bloodpengu_python"],
-    package_data={
-        "modules": ["*.py"],
-    },
-    entry_points={
-        "console_scripts": [
-            "bloodpengu-python=bloodpengu_python:main",
-        ],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: Apache Software License",
-        "Environment :: Console",
-        "Topic :: Security",
-    ],
-)
+setup()
